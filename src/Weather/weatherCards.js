e279a9b0a4d03529059a851c15b9f8836999a453
Dan <dtan9988@gmail.com>
2020-11-14 22:14:01 -0800

    edit

--- src/Weather/weatherCards.js
+++ src/Weather/weatherCards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Grid, Card, CardContent, CardMedia, CardActions, CardActionArea, Typography, Collapse, IconButton} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Data from './data/weatherCards.json'
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -16,13 +17,13 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export const WeatherCards = ({data : {current}}) => {
+export const WeatherCards = ({ data : {current} }) => {
   const classes = useStyles();
 
   if (current) {
     return (
-      <Grid container spacing={3} justify='start'>
-        <Grid item component={Card} xs={5} md={2} className={classes.card}>
+    <Grid container spacing={3} justify='start'>
+      <Grid item component={Card} xs={5} md={2} className={classes.card}>
         <CardMedia
           component='img'
           image='temperature.png'
@@ -32,41 +33,41 @@ export const WeatherCards = ({data : {current}}) => {
             <Typography color='textSecondary' gutterBottom> Current Temperature </Typography>
             <Typography variant='h5'> {current.temp} </Typography>
           </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={5} md={2} className={classes.card}>
-          <CardMedia
-            component='img'
-            image='humidity.png'
-            height='120'
-          />
-          <CardContent>
-            <Typography color='textSecondary' gutterBottom> Current Humidity </Typography>
-            <Typography variant='h5'> {current.humidity} </Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={5} md={2} className={classes.card}>
-          <CardMedia
-            component='img'
-            image='anemometer.png'
-            height='120'
-          />
-          <CardContent>
-            <Typography color='textSecondary' gutterBottom> Current Wind Speed </Typography>
-            <Typography variant='h5'> {current.wind_speed} </Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={5} md={2} className={classes.card}>
-          <CardMedia
-            component='img'
-            image='barometer.png'
-            height='120'
-          />
-          <CardContent>
-            <Typography color='textSecondary' gutterBottom> Current Pressure </Typography>
-            <Typography variant='h5'> {current.pressure} </Typography>
-          </CardContent>
-        </Grid>
       </Grid>
+      <Grid item component={Card} xs={5} md={2} className={classes.card}>
+        <CardMedia
+          component='img'
+          image='humidity.png'
+          height='120'
+        />
+        <CardContent>
+          <Typography color='textSecondary' gutterBottom> Current Humidity </Typography>
+          <Typography variant='h5'> {current.humidity} </Typography>
+        </CardContent>
+      </Grid>
+      <Grid item component={Card} xs={5} md={2} className={classes.card}>
+        <CardMedia
+          component='img'
+          image='anemometer.png'
+          height='120'
+        />
+        <CardContent>
+          <Typography color='textSecondary' gutterBottom> Current Wind Speed </Typography>
+          <Typography variant='h5'> {current.wind_speed} </Typography>
+        </CardContent>
+      </Grid>
+      <Grid item component={Card} xs={5} md={2} className={classes.card}>
+        <CardMedia
+          component='img'
+          image='barometer.png'
+          height='120'
+        />
+        <CardContent>
+          <Typography color='textSecondary' gutterBottom> Current Pressure </Typography>
+          <Typography variant='h5'> {current.pressure} </Typography>
+        </CardContent>
+      </Grid>
+    </Grid>
     )
   } else {
     return null;
